56236c0c34ac74ffccd1bde2f3ddcd2fff8cdb7f
Roman <romaholovai@gmail.com>
2025-02-15 16:22:06 +0100

    project created

---
+++ RickAndMorty/RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Roman on 15/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
